0af5583875ec183e6cf6f8d106be96323a61c191
Tobias <Tobias@10.16.161.221>
2015-03-24 11:44:18 +0100

    localshit

--- src/daoimpl01917/MySQLProduktBatchKompDAO.java
+++ src/daoimpl01917/MySQLProduktBatchKompDAO.java
@@ -1,48 +1,85 @@
 package daoimpl01917;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import connector01917.Connector;
 import daointerfaces01917.DALException;
 import daointerfaces01917.ProduktBatchDAO;
 import daointerfaces01917.ProduktBatchKompDAO;
+import dto01917.OperatoerDTO;
 import dto01917.ProduktBatchDTO;
 import dto01917.ProduktBatchKompDTO;
 
 public class MySQLProduktBatchKompDAO implements ProduktBatchKompDAO {
 
 	@Override
-	public ProduktBatchKompDTO getProduktBatchKomp(int pbId, int rbId)
-			throws DALException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<ProduktBatchKompDTO> getProduktBatchKompList(int pbId)
-			throws DALException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public ProduktBatchKompDTO getProduktBatchKomp(int pbId, int rbId) throws DALException{
+	ResultSet rs = Connector.doQuery("SELECT * FROM produktbatchkomp WHERE pb_id = " + pbId + " and rb_id = " + rbId);
+    try {
+    	if (!rs.first()) throw new DALException("Produktbatchkomp " + pbId + " findes ikke");
+    	return new ProduktBatchKompDTO (rs.getInt(pbId), rs.getInt(rbId), rs.getDouble("tara"), rs.getDouble("netto"), rs.getInt("opr_id"));
+    }
+    catch (SQLException e) {throw new DALException(e); }
+	
+}
 
 	@Override
 	public List<ProduktBatchKompDTO> getProduktBatchKompList()
 			throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		List<ProduktBatchKompDTO> list = new ArrayList<ProduktBatchKompDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatchkomp");
+		try
+		{
+			while (rs.next()) 
+			{
+				list.add(new ProduktBatchKompDTO(rs.getInt("pb_Id"), rs.getInt("rb_id"), rs.getDouble("tara"), rs.getDouble("netto"), rs.getInt("opr_id")));
+			}
+		}
+		catch (SQLException e) { throw new DALException(e); }
+		return list;
 	}
+	
+
 
 	@Override
 	public void createProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent)
 			throws DALException {
-		// TODO Auto-generated method stub
+		Connector.doUpdate(
+				"INSERT INTO produktbatchkomp(pb_id, rb_id, tara, netto, opr_id) VALUES " +
+				"(" + produktbatchkomponent.getPbId() + ", " + produktbatchkomponent.getRbId() + ", " + produktbatchkomponent.getTara() + ", " + 
+				produktbatchkomponent.getNetto() + ", " + produktbatchkomponent.getOprId() + ")"
+			);
 
 	}
 
 	@Override
 	public void updateProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent)
 			throws DALException {
-		// TODO Auto-generated method stub
+		Connector.doUpdate(
+				"UPDATE produktbatchkomp SET  pb_id = " + produktbatchkomponent.getPbId() + ", rb_id =  " + produktbatchkomponent.getRbId() + 
+				", tara = " + produktbatchkomponent.getTara() + ", netto = " + produktbatchkomponent.getNetto() + " WHERE pb_id = " +
+				produktbatchkomponent.getPbId()
+		);
 
+	}
+
+	@Override
+	public List<ProduktBatchKompDTO> getProduktBatchKompList(int pbId)
+			throws DALException {
+		List<ProduktBatchKompDTO> list = new ArrayList<ProduktBatchKompDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatchkomp");
+		try
+		{
+			while (rs.next()) 
+			{
+				list.add(new ProduktBatchKompDTO(rs.getInt(pbId), rs.getInt("rb_id"), rs.getDouble("tara"), rs.getDouble("netto"), rs.getInt("opr_id")));
+			}
+		}
+		catch (SQLException e) { throw new DALException(e); }
+		return list;
 	}
 
 }
--- src/daoimpl01917/MySQLReceptKompDAO.java
+++ src/daoimpl01917/MySQLReceptKompDAO.java
@@ -1,44 +1,80 @@
 package daoimpl01917;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import connector01917.Connector;
 import daointerfaces01917.DALException;
 import daointerfaces01917.ReceptKompDAO;
+import dto01917.ProduktBatchKompDTO;
 import dto01917.ReceptKompDTO;
 
 class MySQLReceptKompDAO implements ReceptKompDAO {
 
 	@Override
 	public ReceptKompDTO getReceptKomp(int receptId, int raavareId)
-			throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+	throws DALException {
+	ResultSet rs = Connector.doQuery("SELECT * FROM ReceptKompDTO WHERE recept_id = " + receptId + " and raavare_id = " + raavareId);
+    try {
+    	if (!rs.first()) throw new DALException("RecemptKomp" + receptId + " findes ikke");
+    	return new ReceptKompDTO (rs.getInt(receptId), rs.getInt(raavareId), rs.getDouble("nom_netto"), rs.getDouble("tolerance"));
+    }
+    catch (SQLException e) {throw new DALException(e); }
 	}
 
 	@Override
 	public List<ReceptKompDTO> getReceptKompList(int receptId)
 			throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		List<ReceptKompDTO> list = new ArrayList<ReceptKompDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM Receptkomp");
+		try
+		{
+			while (rs.next()) 
+			{
+				list.add(new ReceptKompDTO(rs.getInt(receptId), rs.getInt("raavare_Id"), rs.getDouble("nom_netto"), rs.getDouble("tolerance")));
+			}
+		}
+		catch (SQLException e) { throw new DALException(e); }
+		return list;
 	}
 
 	@Override
 	public List<ReceptKompDTO> getReceptKompList() throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		List<ReceptKompDTO> list = new ArrayList<ReceptKompDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM Receptkomp");
+		try
+		{
+			while (rs.next()) 
+			{
+				list.add(new ReceptKompDTO(rs.getInt("recept_Id"), rs.getInt("raavare_Id"), rs.getDouble("nom_netto"), rs.getDouble("tolerance")));
+			}
+		}
+		catch (SQLException e) { throw new DALException(e); }
+		return list;
 	}
 
 	@Override
 	public void createReceptKomp(ReceptKompDTO receptkomponent)
 			throws DALException {
-		// TODO Auto-generated method stub
+		Connector.doUpdate(
+				"INSERT INTO receptkomp(recept_id, raavare_id, nom_netto, tolerance) VALUES " +
+				"(" + receptkomponent.getReceptId() + ", " + receptkomponent.getRaavareId() + ", " + receptkomponent.getNomNetto() + ", " + 
+				receptkomponent.getTolerance()  + ")"
+			);
 
 	}
 
 	@Override
 	public void updateReceptKomp(ReceptKompDTO receptkomponent)
 			throws DALException {
-		// TODO Auto-generated method stub
+		Connector.doUpdate(
+				"INSERT INTO receptkomp(recept_id, raavare_id, nom_netto, tolerance) VALUES " +
+						"(" + receptkomponent.getReceptId() + ", " + receptkomponent.getRaavareId() + ", " + receptkomponent.getNomNetto() + ", " + 
+						receptkomponent.getTolerance()  +  "' WHERE recept_id = " + receptkomponent.getReceptId()
+		);
+
 
 	}
 
