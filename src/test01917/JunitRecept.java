e19c8574aa3243f20e7bbb12d039e7699faa92b8
Tobias <Tobias@10.16.162.37>
2015-04-28 10:59:23 +0200

    Junit

---
+++ src/test01917/JunitRecept.java
@@ -0,0 +1,72 @@
+package test01917;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import junit.framework.Assert;
+import connector01917.Connector;
+import daoimpl01917.MySQLOperatoerDAO;
+import daoimpl01917.MySQLReceptDAO;
+import daointerfaces01917.DALException;
+import dto01917.OperatoerDTO;
+import dto01917.ReceptDTO;
+
+
+public class JunitRecept {
+
+	
+	@Test
+	public void test() {
+	
+
+				try { new Connector(); } 
+				catch (InstantiationException e) { e.printStackTrace(); }
+				catch (IllegalAccessException e) { e.printStackTrace(); }
+				catch (ClassNotFoundException e) { e.printStackTrace(); }
+				catch (SQLException e) { e.printStackTrace(); }
+				
+				System.out.println("Recept nummer 1:");
+				MySQLReceptDAO receptDAO = new MySQLReceptDAO();
+				try { System.out.println(receptDAO.getRecept(1)); }
+				catch (DALException e) { System.out.println(e.getMessage()); }
+
+				
+				System.out.println("Recept nummer 2:");
+				try { System.out.println(receptDAO.getRecept(2)); }
+				catch (DALException e) { System.out.println(e.getMessage()); }
+				
+				System.out.println("Recept nummer 3:");
+				try { System.out.println(receptDAO.getRecept(3)); }
+				catch (DALException e) { System.out.println(e.getMessage()); }
+				
+				System.out.println("Indsættelse af Recept Fiskefrikedeller");
+				ReceptDTO newRecept = new ReceptDTO(10,"Fiskefrikedeller");
+				try { 
+				
+					receptDAO.createRecept(newRecept);
+					newRecept = receptDAO.getLatestRecept();				
+					assertEquals(newRecept.getReceptNavn(),"Fiskefrikedeller");
+				}
+				catch (DALException e) { System.out.println(e.getMessage()); }
+				
+				
+				System.out.println("Opdater receptnavn på " + newRecept.getReceptId());
+				newRecept.setReceptNavn("pizza");
+				try { 
+					receptDAO.updateRecept(newRecept);
+					System.out.println(receptDAO.getRecept(newRecept.getReceptId()));
+				}
+				catch (DALException e) { System.out.println(e.getMessage()); }
+				
+				
+				
+			}
+		
+
+		
+	}
+
+
